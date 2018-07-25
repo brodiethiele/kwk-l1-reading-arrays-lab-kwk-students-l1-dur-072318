711867fa9fabcfed7818277ae8a1d6cdb098fb05
brodiethiele <brodie.thiele@gmail.com>
2018-07-25 13:08:15 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -24,14 +24,17 @@ end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  return student_names.first
 end
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+  # Write a solution that returns the first student using the built in .last 
+  return student_names.last
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
+  return student_names[0,1,2]
 end
 first_student_by_index
 fourth_student_by_index
